fcb5fd50566136cb9ccb1d8bfb99b6f4e4faa1de
Ying Xiong <yxiong@seas.harvard.edu>
2014-12-06 22:28:53 -0500

    Refactor: create include/ src/ test/ folders, and move corresponding source files

--- test/CommandLineFlagsTest.sh
+++ test/CommandLineFlagsTest.sh
@@ -4,15 +4,15 @@
 # Created: Dec 05, 2014.
 
 set -x -e
-exe_path=$1
+project_binary_path=$1
 data_path=$2
 
-${exe_path}/CommandLineFlagsTest                               \
+${project_binary_path}/test/CommandLineFlagsTest               \
            --long_test=123456789 arg1 --string_test="foo bar"  \
            --int_test=0xff arg2 --bool_test --char_test=c      \
            --double_test=23.34 arg3
 
-${exe_path}/CommandLineFlagsTest                               \
+${project_binary_path}/test/CommandLineFlagsTest               \
            --long_test=123456789 arg1 arg2 arg3                \
            --flagfile=${data_path}/Texts/CommandLineFlagsFileTest.txt \
            arg4 --char_test=c arg5
--- test/visualize_points_and_cameras_test.sh
+++ test/visualize_points_and_cameras_test.sh
@@ -4,10 +4,10 @@
 # Created: Dec 04, 2014.
 
 set -x -e
-exe_path=$1
+project_binary_path=$1
 data_path=$2
 
-${exe_path}/visualize_points_and_cameras                    \
+${project_binary_path}/bin/visualize_points_and_cameras     \
     --pointFileType=ply                                     \
     --pointFile=${data_path}/Models/dinoSparseRing-pmvs.ply \
     --cameraFileType=NumNameKRt                             \
